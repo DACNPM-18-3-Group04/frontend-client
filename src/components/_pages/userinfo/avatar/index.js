48ad389a46909d819423ab983922371630b1ef26
Nguyen Cong Dat <nguyencongdat@Nguyens-MacBook-Air.local>
2022-04-10 12:45:29 +0700

    update component avatar

--- src/components/_pages/userinfo/avatar/index.js
+++ src/components/_pages/userinfo/avatar/index.js
@@ -1,14 +1,21 @@
 import { Avatar, IconButton, Typography } from '@mui/material';
 import EditIcon from '@mui/icons-material/Edit';
 import { Box } from '@mui/system';
+import UploadFile from '../../uploadFile';
+import { useState } from 'react';
 
 export default function AvatarInfo(props) {
+
+  const [open, setOpen] = useState(false);
   const { avatar, fullname } = props;
   return (
     <div>
       <Box>
+        <UploadFile
+        open={open}></UploadFile>
         <IconButton onClick={()=>{
             console.log("Change avatar")
+            setOpen(true)
         }}>
           <Avatar alt={fullname} src={avatar}>
             {fullname ? fullname.charAt(0) : null}
